704a583eb8d199c85d9f8e9f94d4f7896eabce94
AlbertSu2015 <albertsu2001@gmail.com>
2018-05-09 10:38:02 -0700

    Update ComputerPlayer.java

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -2,58 +2,67 @@ package connect4;
 
 public class ComputerPlayer extends Player
 {
-    int computerPlayerCoins;
-    int humanPlayerCoins;
-    int bidAmount;
-    int computerMovesToWin;
-    int humanMovesToWin;
-    public static int getBidAmount()
+    static ConnectFour test = new ConnectFour();
+    static int computerPlayerCoins;
+    static int humanPlayerCoins;
+    static int bidAmount;
+    static int computerMovesToWin;
+    static int humanMovesToWin;
+    
+    public static int humanPlayerCoins()
     {
-        return bidAmount;
-        
+        humanPlayerCoins = test.getYellowCoins();
+        return humanPlayerCoins;
     }
-    public int bidAmount()
+    
+    public static int computerPlayerCoins()
     {
-        if((computerMovesToWin == 1) && (computerPlayerCoins > humanPlayerCoins))
+        computerPlayerCoins = test.getRedCoins();
+        return computerPlayerCoins;
+    }
+    
+    public static int bidAmount()
+    {
+        if((computerMovesToWin == 1) && (computerPlayerCoins() > humanPlayerCoins()))
         {
-            bidAmount = humanPlayerCoins + 1;
+            bidAmount = humanPlayerCoins() + 1;
         }
         
-        if((computerMovesToWin == 2) && (computerPlayerCoins > ((2 *humanPlayerCoins) + 1)))
+        if((computerMovesToWin == 2) && (computerPlayerCoins() > ((2 *humanPlayerCoins()) + 1)))
         {
-            bidAmount = humanPlayerCoins + 1;
+            bidAmount = humanPlayerCoins() + 1;
         }
         
-        if((computerMovesToWin == 3) && (computerPlayerCoins > ((3 *humanPlayerCoins) + 1)))
+        if((computerMovesToWin == 3) && (computerPlayerCoins() > ((3 *humanPlayerCoins()) + 1)))
         {
-            bidAmount = humanPlayerCoins + 1;
+            bidAmount = humanPlayerCoins() + 1;
         }
         
-        if((computerMovesToWin >= 4) && (computerPlayerCoins > ((4 *humanPlayerCoins) + 1)))
+        if((computerMovesToWin >= 4) && (computerPlayerCoins() > ((4 *humanPlayerCoins()) + 1)))
         {
-            bidAmount = humanPlayerCoins + 1;
+            bidAmount = humanPlayerCoins() + 1;
         }
         
-        if((computerMovesToWin == 2) && (computerPlayerCoins > ((2 *humanPlayerCoins) + 1)))
+        if((computerMovesToWin == 2) && (computerPlayerCoins() > ((2 *humanPlayerCoins()) + 1)))
         {
-            bidAmount = humanPlayerCoins + 1;
+            bidAmount = humanPlayerCoins() + 1;
         }
         
         if ((humanMovesToWin == 1)) //if the human is one step from winning, stop him at all costs!
         {
-            if(computerPlayerCoins > humanPlayerCoins)
+            if(computerPlayerCoins() > humanPlayerCoins())
             {
-                bidAmount = humanPlayerCoins;
+                bidAmount = humanPlayerCoins();
             }
             else
             {
-                bidAmount = computerPlayerCoins;
+                bidAmount = computerPlayerCoins();
             }
         }
         
-        if (bidAmount > computerPlayerCoins)
+        if (bidAmount > computerPlayerCoins())
         {
-            bidAmount = computerPlayerCoins;
+            bidAmount = computerPlayerCoins();
         }
         return bidAmount;
     }
@@ -63,4 +72,5 @@ public class ComputerPlayer extends Player
         
         return computerMovesToWin;
     }
+    
 }
