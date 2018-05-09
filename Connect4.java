2382650d246cdf91c2fba8c31f5d30a77c292483
Arnav Gupta <arnavgupta.k@gmail.com>
2018-05-08 22:24:08 -0700

    Update Connect4.java

--- Connect4.java
+++ Connect4.java
@@ -1,35 +1,42 @@
 package connect4;
 import java.util.Scanner;
 
+
 public class ConnectFour
 {
-    public static int yellow = 0;
-    public static int red = 1;
-    public static int tie = 1;
-    public static int yellowCoins = 100;
-    public static int redCoins = 100;
+
 public static String[][] createPattern()
+
 {
-   String[][] f = new String[7][15];
+
+
+  String[][] f = new String[7][15];
   for (int i =0;i<f.length;i++)
+
   {
      for (int j =0;j<f[i].length;j++)
     {
-      if (j% 2 == 0)
+      if (j% 2 == 0) 
       {
           f[i][j] ="|";
       }
-      else
+
+      else 
       {
           f[i][j] = " ";
       }
-      if (i==6) 
-      {
-          f[i][j]= "-";
-      }
+
+
+
+      if (i==6) f[i][j]= "-";
+
     }
+
+
   }
+
   return f;
+
 }
 
 
@@ -45,178 +52,288 @@ public static void printPattern(String[][] f)
     }
     System.out.println();
   }
-}
-
-
-public static int getNextPlayer()
-{
-    System.out.println("How much to bid(red): ");
-    Scanner scan = new Scanner(System.in);
-    int playerRed = scan.nextInt();
-    redCoins = redCoins - playerRed;
-    
-    System.out.println("How much to bid(yellow): ");
-    int playerYellow = scan.nextInt();
-    yellowCoins = yellowCoins - playerYellow;
-    if(playerYellow > playerRed)
-    {
-        return yellow;
-    }
-    else if(playerYellow == playerRed)
-    {
-        return tie;
-    }
-    else
-    {
-        return red;
-    }
-
 
 }
 
 public static void dropRedPattern(String[][] f)
 {
+ 
   System.out.println("Drop a red disk at column (0–6): ");
+
   Scanner scan = new Scanner (System.in);
 
   int c = 2*scan.nextInt()+1;
   
   for (int i =5;i>=0;i--)
   {
+
     if (f[i][c] == " ")
     {
       f[i][c] = "R";
       break;
+
     }
+
+
   }
 
 }
-
-
 
 public static void dropYellowPattern(String[][] f)
+
 {
   System.out.println("Drop a yellow disk at column (0–6): ");
+
   Scanner scan = new Scanner (System.in);
+
   int c = 2*scan.nextInt()+1;
+
   for (int i =5;i>=0;i--)
+
   {
+
     if (f[i][c] == " ")
+
     {
+
       f[i][c] = "Y";
+
       break;
+
     }
+
+
   }
 
 }
 
-public static String checkWinner(String[][] f)
+public static int makeBid()
 {
-  for (int i =0;i<6;i++)
-  {
-    for (int j=0;j<7;j+=2)
+ int tokens1 = 100;
+    
+    System.out.println("You currently have red player" + tokens1 + "tokens"
+            + "How much do you want to bid?");
+    
+    Scanner scan = new Scanner (System.in);
+    int bid = scan.nextInt();
+    
+    if(bid > 100)
     {
-      if ((f[i][j+1] != " ")
-      && (f[i][j+3] != " ")
-      && (f[i][j+5] != " ")
-      && (f[i][j+7] != " ")
-      && ((f[i][j+1] == f[i][j+3])
-      && (f[i][j+3] == f[i][j+5])
-      && (f[i][j+5] == f[i][j+7])))
-      return f[i][j+1]; 
+        System.out.println("You don't have that much money!");
     }
+    
+    tokens1 = tokens1 - bid;
+
+    
+    return ;
+   
+}
+
+public static int makeBid2()
+{
+    int tokens2 = 100;
+    
+    System.out.println("You currently have yellow" + tokens2 + "tokens"
+            + "How much do you want to bid?");
+    
+    Scanner scan = new Scanner (System.in);
+    int bid = scan.nextInt();
+    
+    if(bid > 100)
+    {
+        System.out.println("You don't have that much money!");
+    }
+    
+    tokens2 = tokens2 - bid;
+
+    
+    return bid;
+    
+}
+
+
+
+public static String checkWinner(String[][] f)
+
+{
+
+
+  
+  for (int i =0;i<6;i++)
+
+  {
+
+   
+    for (int j=0;j<7;j+=2)
+
+    {
+
+      if ((f[i][j+1] != " ")
+
+      && (f[i][j+3] != " ")
+
+      && (f[i][j+5] != " ")
+
+      && (f[i][j+7] != " ")
+
+      && ((f[i][j+1] == f[i][j+3])
+
+      && (f[i][j+3] == f[i][j+5])
+
+      && (f[i][j+5] == f[i][j+7])))
+
+        return f[i][j+1]; 
+
+    }
+
   }
 
+
+
   for (int i=1;i<15;i+=2)
-  {   
-      for (int j =0;j<3;j++)
-      {
-          if((f[j][i] != " ")
-          && (f[j+1][i] != " ")
-          && (f[j+2][i] != " ")
-          && (f[j+3][i] != " ")
-          && ((f[j][i] == f[j+1][i])
-          && (f[j+1][i] == f[j+2][i])
-          && (f[j+2][i] == f[j+3][i])))
-            return f[j][i]; 
-      } 
-  }
- 
-  for (int i=0;i<3;i++)
+
   {
-      for (int j=1;j<9;j+=2)
-      {
-          if((f[i][j] != " ")
-          && (f[i+1][j+2] != " ")
-          && (f[i+2][j+4] != " ")
-          && (f[i+3][j+6] != " ")
-          && ((f[i][j] == f[i+1][j+2])
-          && (f[i+1][j+2] == f[i+2][j+4])
-          && (f[i+2][j+4] == f[i+3][j+6])))
-            return f[i][j]; 
-    } 
-  }
- 
-  for (int i=0;i<3;i++)
-  {
-    for (int j=7;j<15;j+=2)
+
+    
+    for (int j =0;j<3;j++)
+
     {
-          if((f[i][j] != " ")
-          && (f[i+1][j-2] != " ")
-          && (f[i+2][j-4] != " ")
-          && (f[i+3][j-6] != " ")
-          && ((f[i][j] == f[i+1][j-2])
-          && (f[i+1][j-2] == f[i+2][j-4])
-          && (f[i+2][j-4] == f[i+3][j-6])))
-            return f[i][j]; 
+
+          if((f[j][i] != " ")
+
+          && (f[j+1][i] != " ")
+
+          && (f[j+2][i] != " ")
+
+          && (f[j+3][i] != " ")
+
+          && ((f[j][i] == f[j+1][i])
+
+          && (f[j+1][i] == f[j+2][i])
+
+          && (f[j+2][i] == f[j+3][i])))
+
+            return f[j][i]; 
+
     } 
+
   }
+
+
+ 
+
+  for (int i=0;i<3;i++)
+
+  {
+
+
+  
+    for (int j=1;j<9;j+=2)
+
+    {
+
+          if((f[i][j] != " ")
+
+          && (f[i+1][j+2] != " ")
+
+          && (f[i+2][j+4] != " ")
+
+          && (f[i+3][j+6] != " ")
+
+          && ((f[i][j] == f[i+1][j+2])
+
+          && (f[i+1][j+2] == f[i+2][j+4])
+
+          && (f[i+2][j+4] == f[i+3][j+6])))
+
+            return f[i][j]; 
+
+    } 
+
+  }
+
+
+ 
+  for (int i=0;i<3;i++)
+
+  {
+
+    for (int j=7;j<15;j+=2)
+
+    {
+
+          if((f[i][j] != " ")
+
+          && (f[i+1][j-2] != " ")
+
+          && (f[i+2][j-4] != " ")
+
+          && (f[i+3][j-6] != " ")
+
+          && ((f[i][j] == f[i+1][j-2])
+
+          && (f[i+1][j-2] == f[i+2][j-4])
+
+          && (f[i+2][j-4] == f[i+3][j-6])))
+
+            return f[i][j]; 
+
+    } 
+
+  }
+
+
+ 
   return null;
+
 }
 
 
 
 public static void main (String[] args)
+
 {
+
+
   String[][] f = createPattern();
+
   boolean loop = true;
+
+
   printPattern(f);
+
   while(loop)
+
   {
-     if (getNextPlayer() == yellow) 
+     if(makeBid() > makeBid2())
      {
+        dropRedPattern(f);
+        
+     }
+     else if (makeBid2() > makeBid())
          dropYellowPattern(f);
-         System.out.print( "Yellow has " + Integer.valueOf( yellowCoins ) + " coins left");
-         System.out.print( "Red has " + Integer.valueOf( redCoins ) + " coins left");
-     }
-     else if(getNextPlayer() == tie)
-     {
-         System.out.print("There was a tie");
-         System.out.print( "Yellow has " + Integer.valueOf( yellowCoins ) + " coins left");
-         System.out.print( "Red has " + Integer.valueOf( redCoins ) + " coins left");
-     }
-     else
-     {
-         dropRedPattern(f);
-         System.out.print( "Yellow has " + Integer.valueOf( yellowCoins ) + " coins left");
-         System.out.print( "Red has " + Integer.valueOf( redCoins ) + " coins left");
-     }
+
+
      printPattern(f);
-     
+
+
      if (checkWinner(f) != null)
      {
+
         if (checkWinner(f) == "R")
-        {
-            System.out.println("The red player won.");
-            
-        }           
+
+           System.out.println("The red player won.");
+
         else if (checkWinner(f)== "Y")
-        {
-            System.out.println("The yello player won.");
-        }
+
+          System.out.println("The yellow player won.");
+
        loop = false;
+
   }
-  }
+
+}
+
 }
 
 }
