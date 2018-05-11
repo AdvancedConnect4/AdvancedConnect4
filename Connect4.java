07f57abc59846c80ac4fe8c1a340d8b8df0332ff
Arnav Gupta <arnavgupta.k@gmail.com>
2018-05-11 12:31:05 -0700

    Update Connect4.java

--- Connect4.java
+++ Connect4.java
@@ -1,40 +1,40 @@
 package connect4;
-import java.util.Scanner;
 
 public class ConnectFour
 {
-    static Logic logic = new Logic();
-
     public static int yellow = 0;
     public static int red = 1;
     public static int tie = 2;
-
+    public static int yellowCoins = 100;
+    public static int redCoins = 100;
+    
+static Logic logic = new Logic();
 public static void main (String[] args)
 {
   String[][] grid = logic.createGrid();
   boolean loop = true;
   logic.printGrid(grid);
-  
   while(loop)
   {
       int playerTurn = logic.getNextPlayer();
+      
       if(playerTurn == red)
       {
           logic.dropRedPattern(grid);
-          System.out.println( "Yellow has " + Integer.valueOf( logic.getYellowCoins() ) + " coins left");
-          System.out.println( "Red has " + Integer.valueOf( logic.getRedCoins()) + " coins left");
+          System.out.println( "Yellow has " + Integer.valueOf(yellowCoins) + " coins left");
+          System.out.println( "Red has " + Integer.valueOf(redCoins) + " coins left");
       } 
       else if (playerTurn == yellow) 
       {
          logic.dropYellowPattern(grid);
-         System.out.println( "Yellow has " + Integer.valueOf( logic.getYellowCoins() ) + " coins left");
-         System.out.println( "Red has " + Integer.valueOf( logic.getRedCoins() ) + " coins left");
+         System.out.println( "Yellow has " + Integer.valueOf(yellowCoins ) + " coins left");
+         System.out.println( "Red has " + Integer.valueOf( redCoins ) + " coins left");
      }
      else
      {
          System.out.println("There was a tie");
-         System.out.println( "Yellow has " + Integer.valueOf( logic.getYellowCoins() ) + " coins left");
-         System.out.println( "Red has " + Integer.valueOf( logic.getRedCoins()) + " coins left");
+         System.out.println( "Yellow has " + Integer.valueOf(yellowCoins ) + " coins left");
+         System.out.println( "Red has " + Integer.valueOf( redCoins ) + " coins left");
      }
      logic.printGrid(grid);
      
@@ -53,4 +53,10 @@ public static void main (String[] args)
   }
   }
 }
+
+public int getRedCoins()
+{
+    return redCoins;
+}
+
 }
