72f9f7cbb67e35499f563f7f388a5e252dee208a
AlbertSu2015 <albertsu2001@gmail.com>
2018-05-18 16:53:49 -0700

    Update ComputerPlayer.java

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -6,7 +6,6 @@ public class ComputerPlayer extends Player {
     static int computerPlayerCoins;
     static int humanPlayerCoins;
     static int bidAmount;
-    static int computerMovesToWin;
     static int humanMovesToWin; // how many moves it will take for the computer
                                 // to win
     static int[] previousHumanBids = new int[43]; // keeps track of the previous
@@ -14,96 +13,96 @@ public class ComputerPlayer extends Player {
                                                   // bidded
     static int movesPlayed = 0; // how many moves have been played(not each
                                 // player but total/2)
-
-    public void setPreviousHumanBids() {
-        previousHumanBids = test.getHumanPlayerBids();
+    
+    public void setMovesPlayed() 
+    {
+        movesPlayed = Logic.getMovesPlayed();
     }
-
-    public void setMovesPlayed() {
-        movesPlayed = test.getMovesPlayed();
+    
+    public int getPreviousHumanMoves(int move)
+    {
+        previousHumanBids = Logic.getHumanPlayerBids();
+        return previousHumanBids[move];
     }
-
     public static int humanPlayerCoins() {
-        humanPlayerCoins = test.getYellowCoins();
+        humanPlayerCoins = Logic.getYellowCoins();
         return humanPlayerCoins;
     }
 
     public static int computerPlayerCoins() {
-        computerPlayerCoins = test.getRedCoins();
+        computerPlayerCoins = Logic.getRedCoins();
         return computerPlayerCoins;
+    }
+    
+    public int computerMovesToWin() {
+        return 3;
     }
 
     public String humanMovesToWin() {
-        if (test.checkThreeInaRow(connectfour.getGrid()) != null) {
+        if (test.checkYellowThreeInaRow(connectfour.getGrid()) != null) {
             humanMovesToWin = 1;
-            return test.checkThreeInaRow(connectfour.getGrid());
+            return test.checkYellowThreeInaRow(connectfour.getGrid());
         } 
-        else if (test.checkTwoInaRow(connectfour.getGrid()) != null) 
+        else if (test.checkYellowTwoInaRow(connectfour.getGrid()) != null) 
         {
             humanMovesToWin = 2;
-            return test.checkTwoInaRow(connectfour.getGrid());
+            return test.checkYellowTwoInaRow(connectfour.getGrid());
         }
-//        else if()
-//        {
-//            
-//        }
         else{
             humanMovesToWin = 4;
             return connectfour.getMiddlePosition();
         }
     }
 
-    public int bidAmount() {
-       if ((computerMovesToWin == 1)
-                && (computerPlayerCoins() > humanPlayerCoins())) 
+    public int bidAmount() 
+    {
+       if ((computerMovesToWin() == 1)
+           && (computerPlayerCoins() > humanPlayerCoins())) 
        {
-            bidAmount = humanPlayerCoins() + 1;
+            bidAmount = humanPlayerCoins();
         }
-        if ((computerMovesToWin == 2)
+        if ((computerMovesToWin() == 2)
                 && (computerPlayerCoins() > ((2 * humanPlayerCoins()) + 1))) 
         {
-            bidAmount = humanPlayerCoins() + 1;
+            bidAmount = humanPlayerCoins();
         }
-        if ((computerMovesToWin == 3)
+        if ((computerMovesToWin() == 3)
                 && (computerPlayerCoins() > ((3 * humanPlayerCoins()) + 1))) 
         {
-            bidAmount = humanPlayerCoins() + 1;
+            bidAmount = humanPlayerCoins();
         }
-        if ((computerMovesToWin >= 4)
+        if ((computerMovesToWin() >= 4)
                 && (computerPlayerCoins() > ((4 * humanPlayerCoins()) + 1))) 
         {
-            bidAmount = humanPlayerCoins() + 1;
+            bidAmount = humanPlayerCoins();
         }
-        if ((computerMovesToWin == 2)
+        if ((computerMovesToWin() == 2)
                 && (computerPlayerCoins() > ((2 * humanPlayerCoins()) + 1))) 
         {
-            bidAmount = humanPlayerCoins() + 1;
+            bidAmount = humanPlayerCoins();
         }
-        if ((humanMovesToWin == 1)) // if the human is one step from winning,
-                                    // stop him at all costs!
-        {
-            if (computerPlayerCoins() > humanPlayerCoins()) {
-                bidAmount = humanPlayerCoins();
-            } else {
-                bidAmount = computerPlayerCoins() - 5;
-            }
-        }
-        if (humanMovesToWin == 2) {
-            if ((previousHumanBids[movesPlayed]
-                    - previousHumanBids[movesPlayed - 1]) < 5) {
-                bidAmount = previousHumanBids[movesPlayed] + 1;
-            }
-        }
-        if (bidAmount > computerPlayerCoins()) {
-            bidAmount = computerPlayerCoins();
-        }
-        movesPlayed++;
+//        if ((humanMovesToWin == 1)) // if the human is one step from winning,
+//                                    // stop him at all costs!
+//        {
+//            if (computerPlayerCoins() > humanPlayerCoins()) {
+//                bidAmount = humanPlayerCoins();
+//            } else {
+//                bidAmount = computerPlayerCoins() - 5;
+//            }
+//        }
+//        if (humanMovesToWin == 2)
+//        {
+//            if ((getPreviousHumanMoves(movesPlayed)
+//                            - getPreviousHumanMoves(movesPlayed - 1)) < 5) 
+//                    {
+//                        bidAmount = getPreviousHumanMoves(movesPlayed) + 1;
+//                    }
+//        }
 
+        movesPlayed++;
         return bidAmount;
     }
 
-    public int computerMovesToWin() {
-        return computerMovesToWin;
-    }
+
 
 }
