a9550e73bf59ef800481c9dc4f7cb176403751ba
Arnav Gupta <arnavgupta.k@gmail.com>
2018-05-19 11:46:28 -0700

    Update Logic.java

--- Logic.java
+++ Logic.java
@@ -398,7 +398,7 @@ public class Logic
        return null;
    }
    
-   public String checkYellowThreeInaRow(String [][] grid)
+   public int checkYellowThreeInaRow(String [][] grid)
    {
        for ( int row = 0; row < 6; row++ )
        {
@@ -408,7 +408,7 @@ public class Logic
                    && ( grid[row][col + 5] != " " ) && ( grid[row][col + 7] == " ")
                    && ( ( grid[row][col + 1] == "Y")) && ( grid[row][col + 3] == "Y")
                        &&  (grid[row][col + 5] == "Y"))
-                   return grid[row][col + 7];
+                   return col + 7;
            }
        }
 
@@ -420,7 +420,7 @@ public class Logic
                    && ( grid[col + 2][row] != " " ) && (grid[col + 3][row] == " ")
                    && ( ( grid[col][row] == "Y"))
                        && ( grid[col + 1][row] == "Y") &&(grid[col + 2][row] =="Y"))
-                   return grid[col + 3][row];
+                   return col + 3;
            }
        }
 
@@ -432,7 +432,7 @@ public class Logic
                    && ( grid[row + 2][col + 4] != " " ) && (grid[row + 3][col + 6] == " ")
                    && ( ( grid[row][col] == "Y")) &&  (grid[row + 1][col + 2] == "Y")
                        && (grid[row + 2][col + 4] == "Y"))
-                   return grid[row + 3][col + 6];
+                   return col + 6;
            }
        }
 
@@ -444,10 +444,10 @@ public class Logic
                    && ( grid[row + 2][col - 4] != " " ) && (grid[row + 3][col - 6] == " ")
                    && ( ( grid[row][col] == "Y")) && ( grid[row + 1][col - 2] == "Y" )
                        && (grid[row + 2][col - 4]) == "Y")
-                   return grid[row + 3][col - 6];
+                   return col - 6;
            }
        }
-       return null;
+       return 8;
    }
 
 
