81ad919c13e12f37a164a92968a8ad6ddbdeba15
Arnav Gupta <arnavgupta.k@gmail.com>
2018-05-28 20:01:28 -0700

    Update BettingHandler.java

--- BettingHandler.java
+++ BettingHandler.java
@@ -2,46 +2,23 @@ package connect4;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
-/**
- * 
- *  This class handles the amount that the player bets against the computer. The winner will get 
- *  the amount that the loser bet.
- *
- *  @author  Arnav Gupta
- *  @version May 28, 2018
- *  @author  Period: 2
- *  @author  Assignment: FinalProject
- *
- *  @author  Sources: None
- */
-public class BettingHandler
-{
-    private static final int INITIAL_PLAYER_COINS = 100;
-
+public class BettingHandler {
+    private static final int INITIAL_PLAYER_COINS = 100; 
     private static final int INITIAL_COMPUTER_COINS = 100;
-
     private static final int MAX_ALLOWED_GAMES = 3;
-
     private int playerRemainingCoins;
-
     private int computerRemainingCoins;
-
     private int playerBet;
-
     private int computerBet;
-
     private int gamesPlayed;
-
     Queue<String> betsQueue;
 
     /**
      * Constructor for the class, initializes the values of the variables that keep track of 
      * betting and the LinkedList!
      */
-    public BettingHandler()
-    {
+    public BettingHandler () {
         playerRemainingCoins = INITIAL_PLAYER_COINS;
         computerRemainingCoins = INITIAL_COMPUTER_COINS;
         playerBet = 0;
@@ -49,7 +26,7 @@ public class BettingHandler
         gamesPlayed = 0;
         betsQueue = new LinkedList<>();
     }
-
+    
     /**
      * 
      * This is a helper method that decides how much the computer should bid. Since we aren't 
@@ -60,29 +37,28 @@ public class BettingHandler
      * @param max The maximum bid that the computer will bet.
      * @return the amount of coins that the computer will bet.
      */
-    private int randomWithRange( int min, int max )
+    private int randomWithRange(int min, int max)
     {
-        int range = ( max - min ) + 1;
-        return (int)( Math.random() * range ) + min;
+       int range = (max - min) + 1;     
+       return (int)(Math.random() * range) + min;
     }
-
+   
     /**
      * 
      * Displays the game over sign when someone(human or computer) ran out of money for bets
      * @return the message(who ran out of coins
      */
-    public String gameOverMessage()
-    {
+    public String gameOverMessage() {
         String msg = "Betting over! ";
-        if ( gamesPlayed > MAX_ALLOWED_GAMES )
+        if (gamesPlayed >= MAX_ALLOWED_GAMES)
             msg = "maximum allowed games : " + gamesPlayed + " already played ";
-        if ( playerRemainingCoins <= 0 )
+        if (playerRemainingCoins <=0 )
             msg += "player ran out of money ";
-        if ( computerRemainingCoins <= 0 )
+        if (computerRemainingCoins <=0 )
             msg += "computer ran out of money ";
         return msg;
     }
-
+    
     /**
      * 
      * If the player already played 3 games, the loop is broken. The loop also breaks if one player 
@@ -90,19 +66,16 @@ public class BettingHandler
      * @return if the above statements are true, return false, else, return true and continue
      *  the game
      */
-    public boolean bettingOver()
-    {
-        if ( gamesPlayed > MAX_ALLOWED_GAMES || playerRemainingCoins <= 0
-            || computerRemainingCoins <= 0 )
+    public boolean bettingOver() {
+        if (gamesPlayed >= MAX_ALLOWED_GAMES || playerRemainingCoins <= 0 || computerRemainingCoins <= 0)
             return true;
         return false;
     }
-
+    
     public int getPlayerRemainingCoins() {
         return playerRemainingCoins;
     }
     
-
     /**
      * 
      * When the game starts, this is where the messages come from. The first player to go is the
@@ -110,7 +83,7 @@ public class BettingHandler
      * 
      * @return return true if player bet is >= computerBet else return false.
      */
-    public boolean startBetting()
+    public boolean startBetting(int playerBetNumber)
     {
         gamesPlayed++;       
         playerBet = playerBetNumber;
@@ -125,8 +98,6 @@ public class BettingHandler
         return playerFirst;
     }
     
-    
-
     /**
      * 
      * When the game ends(someone gets 4 in a row), the winner will get the coins the loser bet and 
@@ -134,8 +105,7 @@ public class BettingHandler
      * 
      * @param winner The color of the winner "Y" or "R"
      */
-    public void handleWinner( String winner )
-    {
+    public void handleWinner(String winner) {
         String currentBetData = String.format("Game[%d] Winner[%s] PlayerBet[%d] ComputerBet[%d] PlayerBalance[%d] ComputerBalance[%d]",
                 gamesPlayed, winner, playerBet, computerBet, playerRemainingCoins, computerRemainingCoins);
         
@@ -146,19 +116,17 @@ public class BettingHandler
         } else {
             playerRemainingCoins -= playerBet;
             computerRemainingCoins += playerBet;            
-        }            
+        }                    
     }
-
+    
     /**
      * 
      * This displays all the previous bets. This is a helper method.
      */
-    public void displayAllBets()
-    {
-        for ( String betData : betsQueue )
-        {
-            System.out.println( betData );
-        }
+    public void displayAllBets() {                
+        for (String betData : betsQueue) {
+            System.out.println(betData);
+        }        
     }
 
 }
